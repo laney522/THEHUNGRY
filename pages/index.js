d74229e9e7f6fb8af314464d056978ac0a823665
laney522 <pm72222@gmail.com>
2022-03-27 23:16:44 +0900

    Featured

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Featured from '../components/Featured'
 import Footer from '../components/Footer'
 import Navbar from '../components/NavBar'
 import styles from '../styles/Home.module.css'
@@ -12,7 +13,7 @@ export default function Home() {
         <meta name="description" content="Best shop in town" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      homepage
+      <Featured/>
     </div>
   )
 }
